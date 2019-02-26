9146ebce838a4bd1b169e29ae3ff0cfb935a6b82
Brandon Ikeda <brandon_ikeda@hotmail.com>
2019-02-26 14:12:44 -0800

    Add files via upload
    Basic Programs

---
+++ arthur.py
@@ -0,0 +1,9 @@
+character_name = "Arthur"
+character_job = "Cowboy"
+character_age = 35
+is_male = True
+print("There once was a " + character_job +  " named " + character_name + ",")
+print("He was " + str(character_age) + " years old")
+character_name = "John"
+print("He really liked the name " + character_name)
+print("but he didnt like being " + str(character_age))
---
+++ mathstuff.py
@@ -0,0 +1,5 @@
+from math import *
+my_num = -5.7
+print(ceil(3.7))
+print(floor(3.7))
+print(sqrt(36))
