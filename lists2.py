53e14950bfec1c7a76d72296ed0dc04b707a3ff8
Brandon Ikeda <brandon_ikeda@hotmail.com>
2019-02-26 15:18:57 -0800

    Add files via upload
    basic program

---
+++ lists2.py
@@ -0,0 +1,18 @@
+lucky_numbers = [4,8,15,16,23,42]
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Mike", "Jim"]
+friends.sort()
+lucky_numbers.sort()
+lucky_numbers.reverse()
+print(lucky_numbers)
+friends2=friends.copy()
+print(friends2)
+friends.extend(lucky_numbers)
+friends.insert(1, "Kelly")
+print(friends.count("Jim"))
+friends.remove("Jim")
+print(friends)
+friends.pop()
+print(friends)
+print(friends.index("Kevin"))
+friends.clear()
+print(friends)
